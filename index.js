d8c9b0da8bc4fe375230ce6ee14bc2132dc15096
Miguel Dorta <dortamiguel@gmail.com>
2017-02-13 12:19:20 +0000

    removed env

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 module.exports = {
 	'extends': ['eslint:recommended'],
 
-	'env': {
-		'browser': true,
-		'es6': true,
-		'node': true,
-	},
-
 	'parserOptions': {
 		'ecmaVersion': 6,
 		'ecmaFeatures': {
